76abbe5ead5f3159fe4f5b24aa81eac07b3e77a3
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-11-14 07:47:38 +0530

    First Commit TailWind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme:
+  
+  {
+    color:{
+     
+    },
+    extend: {
+      colors:{
+        thapa:{
+          400:"#021631",
+        },
+      },
+    
+    },
+  },
+  plugins: [],
+}
